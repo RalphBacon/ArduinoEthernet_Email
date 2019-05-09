bd8f28332ce16c8fc47f75269b5ffd162c2f0973
Ralph Bacon <arduino@jargongeneration.uk>
2019-05-09 13:10:00 +0100

    Rename this to config.h on your PC

---
+++ configDemo.h
@@ -0,0 +1,8 @@
+#ifndef CONFIG_H_
+#define CONFIG_H_
+
+const char mailServer[] = "mail.YOURMAILSERVER.com"; // NB might smtp.xxxxx
+const char fromAddress[] = "MAIL From: <YOUR EMAIL ADDRESS>";
+const char toAddress[] = "RCPT To: <RECIPIENTS EMAIL ADDRESS>";
+
+#endif /* CONFIG_H_ */
